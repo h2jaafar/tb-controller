d94862054d10c64345b7a0a3b0bcd16c2880b876
Hussein <husseinali.jaafar@ryerson.ca>
2023-02-02 17:01:48 -0500

    Combined into one class

--- src/velocity-controller.cpp
+++ src/velocity-controller.cpp
@@ -12,66 +12,37 @@
 
 
 #include "../include/tb-controller/MiniPID.h"
+
 #include "../include/tb-controller/utils.h"
 
 using namespace std::chrono_literals;
 
-class VelocityPublisher : public rclcpp::Node
-{
-public:
-  VelocityPublisher()
-  : Node("velocity_publisher"), count_(0)
-  {
-    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&VelocityPublisher::timer_callback, this));
-  }
+MiniPID pid(1,0,0);
 
-private:
-  void timer_callback()
-  {
-    geometry_msgs::msg::Twist vel;
-
-    vel.linear.x = 2;
-    vel.linear.y = 0;
-    vel.linear.z = 0;
-    vel.angular.z = 0.6;
-  
-    // velStamped.header.stamp = rclcpp::Node::now();
-
-    // velStamped.twist = vel;
-    RCLCPP_INFO(this->get_logger(), "Publishing vel");
-    publisher_->publish(vel);
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-  
-  size_t count_;
-};
-
-
-MiniPID pid_x(1,0,0);
-MiniPID pid_y(1,0,0);
-
-class MinimalSubscriber : public rclcpp::Node
+class VelocityController : public rclcpp::Node
 {
   public:
   double global_radius = 5.0;
   double local_radius = 0.2;
   double origin_x = 0.0;
   double origin_y = 0.0;
-    MinimalSubscriber()
-    : Node("minimal_subscriber")
+
+    VelocityController()
+    : Node("velocity_controller")
     {
       subscription_ = this->create_subscription<geometry_msgs::msg::TransformStamped>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
+      "topic", 10, std::bind(&VelocityController::topic_callback, this, std::placeholders::_1));
+
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&VelocityController::timer_callback, this));
     }
 
   private:
-    void topic_callback(const geometry_msgs::msg::TransformStamped::SharedPtr msg) const
+    double output=0.0;
+    void topic_callback(const geometry_msgs::msg::TransformStamped::SharedPtr msg)
     {
-      // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
 
       double x = msg->transform.translation.x;
       double y = msg->transform.translation.y;
@@ -81,7 +52,7 @@ class MinimalSubscriber : public rclcpp::Node
 
 
       std::cout << "x: "<< x << " y: " << y << "z: " << z << std::endl;
-      // Find next waypoint and move to that waypoint
+      // *Find next waypoint and move to that waypoint
       
       // 1 = major circle
       // 2 = minor circle
@@ -90,15 +61,32 @@ class MinimalSubscriber : public rclcpp::Node
 
       // PID
       double target_x = next_waypoint.x;
-      double target_y = next_waypoint.y;
 
-      // ! TODO: figure out how to setup PID's ... which one takes which inputs etc.
-      double output_th=pid_x.getOutput(x,target_x);
-      double output_th_2=pid_y.getOutput(y,target_y);
+      output=pid.getOutput(x,target_x);
 
-      // std::cout << "th: " << th << " output_th: " << output_th << " target_th: " << target_th << std::endl;
     }
+
     rclcpp::Subscription<geometry_msgs::msg::TransformStamped>::SharedPtr subscription_;
+    // Publisher callback
+    void timer_callback()
+    {
+    geometry_msgs::msg::Twist vel;
+
+    vel.linear.x = 2;
+    vel.linear.y = 0;
+    vel.linear.z = 0;
+    vel.angular.z = output;
+  
+    // velStamped.header.stamp = rclcpp::Node::now();
+
+    // velStamped.twist = vel;
+    RCLCPP_INFO(this->get_logger(), "Publishing vel");
+    publisher_->publish(vel);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  
+    size_t count_;
 };
 
 
@@ -107,11 +95,8 @@ int main(int argc, char ** argv)
 {
 
   printf("tb-controller package\n");
-  std::vector<utils::Pose2> X_ref_traj;
-  X_ref_traj = utils::X_ref(5.0, 0.0, 0.0);
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<VelocityPublisher>());
-  rclcpp::spin(std::make_shared<MinimalSubscriber>()); // ? Not sure if this works
+  rclcpp::spin(std::make_shared<VelocityController>());
   rclcpp::shutdown();
   return 0;
 }
